8daa68b8d7f3e1e45b12a8d76b00939658bebddf
ferneynava <ferneynava@gmail.com>
2023-05-13 12:12:01 -0500

    :alien: [Update] code due to external API
    [Update] code due to external API

---
+++ src/assets/main.js
@@ -0,0 +1,41 @@
+/* eslint-disable array-callback-return */
+import { TOKEN } from './Token.js'
+const BASE_API = 'https://graph.instagram.com/me'
+const ACCESS_TOKEN = TOKEN
+
+const photoProfile = document.querySelector('.story')
+const username = document.querySelector('#username')
+const posts = document.querySelector('#posts')
+const photos = document.querySelector('#photos')
+
+async function getUserInfo () {
+  const response = await fetch(`${BASE_API}?fields=username,media_count&access_token=${ACCESS_TOKEN}`)
+  const userInfo = await response.json()
+  console.log(userInfo)
+  username.innerHTML = userInfo.username
+  posts.innerHTML = userInfo.media_count
+  return userInfo
+}
+
+getUserInfo()
+
+async function getUserMediaInfo () {
+  const response = await fetch(`${BASE_API}/media?fields=id,media_url&access_token=${ACCESS_TOKEN}`)
+  const userMediaInfo = await response.json()
+  return userMediaInfo
+}
+
+getUserMediaInfo().then(media => {
+  const imgProfile = document.createElement('img')
+  imgProfile.classList.add('profile-picture')
+  imgProfile.src = media.data[3]?.media_url
+  imgProfile.alt = 'FerneyDev'
+  photoProfile.appendChild(imgProfile)
+
+  media.data.map((mediaInfo) => {
+    const img = document.createElement('img')
+    img.style.width = '100px'
+    img.src = mediaInfo.media_url
+    photos.appendChild(img)
+  })
+})
